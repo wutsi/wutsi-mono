a301064300c482466b13cb2fea7626867044a36c
wutsi <herve.tchepannou@wutsi.com>
2023-03-21 13:25:32 -0400

    setup db

--- initdb.sql
+++ initdb.sql
@@ -1,9 +1,9 @@
-# Script run by CI/CD to create the databases
-
 DROP IF EXISTS membership;
 DROP IF EXISTS marketplace;
+DROP IF EXISTS security;
 DROP IF EXISTS checkout;
 
 CREATE DATABASE membership;
 CREATE DATABASE marketplace;
+CREATE DATABASE security;
 DROP IF EXISTS checkout;
