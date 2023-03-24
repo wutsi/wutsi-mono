5122a31edcde4dc729ceb9a90b475ca70b3589dd
wutsi <herve.tchepannou@wutsi.com>
2023-03-24 17:30:50 -0400

    fix 403 error

--- app/wutsi-web/src/test/kotlin/com/wutsi/application/web/endpoint/SeleniumTestSupport.kt
+++ app/wutsi-web/src/test/kotlin/com/wutsi/application/web/endpoint/SeleniumTestSupport.kt
@@ -62,6 +62,7 @@ abstract class SeleniumTestSupport {
         options.addArguments("--disable-web-security") // To prevent CORS issues
         options.addArguments("--lang=en")
         options.addArguments("--allowed-ips=")
+        options.addArguments("--remote-allow-origins=*")
         options.addArguments("--user-agent =Mozilla/5.0 (iPhone; CPU iPhone OS 10_3 like Mac OS X) AppleWebKit/602.1.50 (KHTML, like Gecko) CriOS/56.0.2924.75 Mobile/14E5239e Safari/602.1")
         if (System.getProperty("headless") == "true") {
             options.addArguments("--headless")
