cc32f4f09e1207cf3c4004f1eaa17336af1e9116
wutsi <herve.tchepannou@wutsi.com>
2023-03-30 16:06:12 -0400

    update api

--- sdk/marketplace-access-sdk-kotlin/src/main/kotlin/com/wutsi/marketplace/access/dto/GetFundraisingResponse.kt
+++ sdk/marketplace-access-sdk-kotlin/src/main/kotlin/com/wutsi/marketplace/access/dto/GetFundraisingResponse.kt
@@ -1,5 +1,5 @@
 package com.wutsi.marketplace.access.dto
 
 public data class GetFundraisingResponse(
-    public val store: Fundraising = Fundraising(),
+    public val fundraising: Fundraising = Fundraising(),
 )
