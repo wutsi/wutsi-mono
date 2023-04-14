fdaa5fcf762d79e3c7357d34f33e8d33681d47ef
wutsi <herve.tchepannou@wutsi.com>
2023-04-13 20:39:37 -0400

    fix klint issue

--- server/checkout-manager-server/src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchDonationKpiDelegate.kt
+++ server/checkout-manager-server/src/main/kotlin/com/wutsi/checkout/manager/delegate/SearchDonationKpiDelegate.kt
@@ -1,4 +1,4 @@
-package com.wutsi.checkout.manager.`delegate`
+package com.wutsi.checkout.manager.delegate
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.checkout.access.CheckoutAccessApi
