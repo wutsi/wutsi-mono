322f5c62ef6fc0a57ee30b60e7f8d8d448f8147b
wutsi <herve.tchepannou@wutsi.com>
2023-05-24 16:14:26 -0400

    fix url issue

--- app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/story/service/HtmlImageService.kt
+++ app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/story/service/HtmlImageService.kt
@@ -5,24 +5,27 @@ import com.wutsi.platform.core.image.ImageService
 import com.wutsi.platform.core.image.Transformation
 import com.wutsi.platform.core.storage.StorageService
 import org.springframework.stereotype.Service
+import java.net.MalformedURLException
 import java.net.URL
 
 @Service
-class HtmlImageService(private val transform: StorageService, private val imageKit: ImageService) {
+class HtmlImageService(private val storage: StorageService, private val imageKit: ImageService) {
     fun sizes() = ""
 
     fun srcset(url: String): String {
-        if (url.startsWith("data") || !transform.contains(URL(url))) {
+        try {
+            if (url.startsWith("data") || !storage.contains(URL(url))) {
+                return ""
+            }
+
+            return widths()
+                .map { imageKit.transform(url, Transformation(Dimension(width = it.toInt()))) + " ${it}w" }
+                .joinToString()
+        } catch (e: MalformedURLException) {
             return ""
         }
-
-        return widths()
-            .map { imageKit.transform(url, Transformation(Dimension(width = it.toInt()))) + " ${it}w" }
-            .joinToString()
     }
 
     // See https://getbootstrap.com/docs/4.0/layout/overview/
-    private fun medias() = arrayListOf("320", "640", "1024")
-
     private fun widths() = arrayListOf("320", "640", "1024")
 }
