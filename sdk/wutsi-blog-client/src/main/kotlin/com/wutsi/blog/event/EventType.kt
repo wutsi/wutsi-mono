d3a7cbfebbbc102a09024ad930e4ed72ac59fcac
wutsi <herve.tchepannou@wutsi.com>
2023-06-01 06:50:30 -0400

    update event urns

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/event/EventType.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/event/EventType.kt
@@ -2,30 +2,30 @@ package com.wutsi.blog.event
 
 object EventType {
     // Command
-    const val COMMENT_STORY_COMMAND = "urn:wutsi:command:comment-story"
+    const val COMMENT_STORY_COMMAND = "urn:wutsi:blog:command:comment-story"
 
-    const val LIKE_STORY_COMMAND = "urn:wutsi:command:like-story"
-    const val UNLIKE_STORY_COMMAND = "urn:wutsi:command:unlike-story"
+    const val LIKE_STORY_COMMAND = "urn:wutsi:blog:command:like-story"
+    const val UNLIKE_STORY_COMMAND = "urn:wutsi:blog:command:unlike-story"
 
-    const val PIN_STORY_COMMAND = "urn:wutsi:command:pin-story"
-    const val UNPIN_STORY_COMMAND = "urn:wutsi:command:unpin-story"
+    const val PIN_STORY_COMMAND = "urn:wutsi:blog:command:pin-story"
+    const val UNPIN_STORY_COMMAND = "urn:wutsi:blog:command:unpin-story"
 
-    const val SHARE_STORY_COMMAND = "urn:wutsi:command:share-story"
+    const val SHARE_STORY_COMMAND = "urn:wutsi:blog:command:share-story"
 
-    const val SUBSCRIBE_COMMAND = "urn:wutsi:command:subscribe"
-    const val UNSUBSCRIBE_COMMAND = "urn:wutsi:command:unsubscribe"
+    const val SUBSCRIBE_COMMAND = "urn:wutsi:blog:command:subscribe"
+    const val UNSUBSCRIBE_COMMAND = "urn:wutsi:blog:command:unsubscribe"
 
     // Event
-    const val STORY_COMMENTED_EVENT = "urn:wutsi:event:story-commented"
+    const val STORY_COMMENTED_EVENT = "urn:wutsi:blog:event:story-commented"
 
-    const val STORY_LIKED_EVENT = "urn:wutsi:event:story-liked"
-    const val STORY_UNLIKED_EVENT = "urn:wutsi:event:story-unliked"
+    const val STORY_LIKED_EVENT = "urn:wutsi:blog:event:story-liked"
+    const val STORY_UNLIKED_EVENT = "urn:wutsi:blog:event:story-unliked"
 
-    const val STORY_PINED_EVENT = "urn:wutsi:event:story-pined"
-    const val STORY_UNPINED_EVENT = "urn:wutsi:event:story-unpined"
+    const val STORY_PINED_EVENT = "urn:wutsi:blog:event:story-pined"
+    const val STORY_UNPINED_EVENT = "urn:wutsi:blog:event:story-unpined"
 
-    const val STORY_SHARED_EVENT = "urn:wutsi:event:story-shared"
+    const val STORY_SHARED_EVENT = "urn:wutsi:blog:event:story-shared"
 
-    const val SUBSCRIBED_EVENT = "urn:wutsi:command:subscribed"
-    const val UNSUBSCRIBED_EVENT = "urn:wutsi:command:unsubscribed"
+    const val SUBSCRIBED_EVENT = "urn:wutsi:blog:command:subscribed"
+    const val UNSUBSCRIBED_EVENT = "urn:wutsi:blog:command:unsubscribed"
 }
