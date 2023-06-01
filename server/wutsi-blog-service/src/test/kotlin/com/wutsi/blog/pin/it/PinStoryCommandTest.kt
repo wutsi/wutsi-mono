2f14ec5938e3be254afaf678bf83a148c8636133
wutsi <herve.tchepannou@wutsi.com>
2023-06-01 06:23:20 -0400

    fix test

--- server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/pin/it/PinStoryCommandTest.kt
+++ server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/pin/it/PinStoryCommandTest.kt
@@ -7,12 +7,12 @@ import com.wutsi.blog.pin.dao.PinStoryRepository
 import com.wutsi.blog.pin.dto.PinStoryCommand
 import com.wutsi.platform.core.stream.Event
 import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.context.jdbc.Sql
 import java.util.Date
-import kotlin.test.assertTrue
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @Sql(value = ["/db/clean.sql", "/db/pin/PinStoryCommand.sql"])
@@ -38,11 +38,14 @@ internal class PinStoryCommandTest {
 
     @Test
     fun pinStory() {
-        // WHEN
+        // GIVEN
         val now = Date()
+        Thread.sleep(1000)
+
+        // WHEN
         pin(100)
 
-        Thread.sleep(20000L)
+        Thread.sleep(15000L)
 
         val story = storyDao.findById(111)
         assertEquals(100, story.get().storyId)
@@ -51,12 +54,15 @@ internal class PinStoryCommandTest {
 
     @Test
     fun pinAnotherStory() {
-        // WHEN
+        // GIVEN
         val now = Date()
+        Thread.sleep(1000)
+
+        // WHEN
         pin(201)
 
         // THEN
-        Thread.sleep(20000L)
+        Thread.sleep(15000L)
 
         val story = storyDao.findById(211)
         assertEquals(201, story.get().storyId)
