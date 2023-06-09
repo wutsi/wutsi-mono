f7b316193194124bb773ad043fa0f2385186d0e4
wutsi <herve.tchepannou@wutsi.com>
2023-06-09 12:57:51 -0400

    add error controller toggle

--- libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/error/spring/ErrorConfiguration.kt
+++ libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/error/spring/ErrorConfiguration.kt
@@ -2,10 +2,16 @@ package com.wutsi.platform.core.error.spring
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.wutsi.platform.core.logging.KVLogger
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
+@ConditionalOnProperty(
+    value = ["wutsi.platform.error.controller-advice.enabled"],
+    havingValue = "true",
+    matchIfMissing = true,
+)
 open class ErrorConfiguration(
     private val logger: KVLogger,
     private val objectMapper: ObjectMapper,
