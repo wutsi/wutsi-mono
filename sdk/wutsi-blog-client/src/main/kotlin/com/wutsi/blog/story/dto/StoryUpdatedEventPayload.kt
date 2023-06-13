6f86b49e6e9efed46b3d7fdd2a8d62e06da88352
wutsi <herve.tchepannou@wutsi.com>
2023-06-13 08:21:18 -0400

    update event schema

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/StoryUpdatedEventPayload.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/StoryUpdatedEventPayload.kt
@@ -3,4 +3,9 @@ package com.wutsi.blog.story.dto
 data class StoryUpdatedEventPayload(
     val title: String? = null,
     val content: String? = null,
+    val summary: String? = null,
+    val topicId: Long? = null,
+    val tags: List<String>? = null,
+    val tagline: String? = null,
+    val access: StoryAccess? = null,
 )
