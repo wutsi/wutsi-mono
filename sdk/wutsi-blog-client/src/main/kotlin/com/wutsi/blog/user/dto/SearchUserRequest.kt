5b45599723c73e3707beb02da10afa72fbc38042
wutsi <herve.tchepannou@wutsi.com>
2023-06-19 21:39:42 -0400

    exclude user-ids

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/user/dto/SearchUserRequest.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/user/dto/SearchUserRequest.kt
@@ -4,12 +4,13 @@ import com.wutsi.blog.SortOrder
 
 data class SearchUserRequest(
     val userIds: List<Long> = emptyList(),
+    val excludeUserIds: List<Long> = emptyList(),
     val limit: Int = 20,
     val offset: Int = 0,
     val sortBy: UserSortStrategy = UserSortStrategy.CREATED,
     val sortOrder: SortOrder = SortOrder.ASCENDING,
     val blog: Boolean? = null,
-    val testUser: Boolean? = false,
+    val testUser: Boolean? = null,
     val active: Boolean? = null,
     val withPublishedStories: Boolean? = null,
 )
