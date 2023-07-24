bb2ea98fd068fece2afd57a74cf26dbd2004c08d
wutsi <herve.tchepannou@wutsi.com>
2023-07-24 10:30:34 -0400

    filter stories kpi by user

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/kpi/dto/SearchStoryKpiRequest.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/kpi/dto/SearchStoryKpiRequest.kt
@@ -1,6 +1,11 @@
 package com.wutsi.blog.kpi.dto
 
+import java.time.LocalDate
+
 data class SearchStoryKpiRequest(
     val types: List<KpiType> = emptyList(),
     val storyIds: List<Long> = emptyList(),
+    val userId: Long? = null,
+    val fromDate: LocalDate? = null,
+    val toDate: LocalDate? = null,
 )
