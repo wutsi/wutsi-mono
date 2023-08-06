87e992b72f310a4aa25a1096e8016d3d79746311
wutsi <herve.tchepannou@wutsi.com>
2023-08-06 14:01:48 -0400

    fix dependencies

--- server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/config/RestConfiguration.kt
+++ server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/config/RestConfiguration.kt
@@ -5,23 +5,29 @@ import com.wutsi.platform.core.security.spring.SpringAuthorizationRequestInterce
 import com.wutsi.platform.core.tracing.TracingContext
 import com.wutsi.platform.core.tracing.spring.SpringTracingRequestInterceptor
 import com.wutsi.platform.core.util.spring.SpringDebugRequestInterceptor
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
 class RestConfiguration(
     private val tracingContext: TracingContext,
     private val tokenProvider: TokenProvider,
+    @Value("\${wutsi.application.backend.connection-timeout}") private val connectionTimeout: Long,
+    @Value("\${wutsi.application.backend.read-timeout}") private val readTimeout: Long,
 ) {
     @Bean
-    fun restTemplate(): RestTemplate {
-        val rest = RestTemplate()
-        rest.interceptors = listOf(
-            SpringDebugRequestInterceptor(),
-            SpringTracingRequestInterceptor(tracingContext),
-            SpringAuthorizationRequestInterceptor(tokenProvider),
-        )
-        return rest
-    }
+    fun restTemplate(): RestTemplate =
+        RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofMillis(connectionTimeout))
+            .setReadTimeout(Duration.ofMillis(readTimeout))
+            .interceptors(
+                SpringDebugRequestInterceptor(),
+                SpringTracingRequestInterceptor(tracingContext),
+                SpringAuthorizationRequestInterceptor(tokenProvider),
+            )
+            .build()
 }
