2e720498aac71bbab499fc760532399fc886d22c
wutsi <herve.tchepannou@wutsi.com>
2023-08-13 08:06:06 -0400

    fix klint

--- server/wutsi-ml-service/src/main/kotlin/com/wutsi/ml/OneOffJobRunner.kt
+++ server/wutsi-ml-service/src/main/kotlin/com/wutsi/ml/OneOffJobRunner.kt
@@ -4,7 +4,7 @@ import com.wutsi.platform.core.cron.CronJobRegistry
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.ExitCodeGenerator
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.SpringApplication
 import org.springframework.context.ApplicationContext
 import org.springframework.stereotype.Service
 
