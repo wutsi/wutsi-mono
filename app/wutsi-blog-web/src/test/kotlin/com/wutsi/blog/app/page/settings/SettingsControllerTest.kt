2930d493a9a23209cdcc563600cb31f5487b28c7
wutsi <herve.tchepannou@wutsi.com>
2023-09-10 09:27:10 -0400

    fix lint issues

--- app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/settings/SettingsControllerTest.kt
+++ app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/settings/SettingsControllerTest.kt
@@ -1,6 +1,8 @@
 package com.wutsi.blog.app.page.settings
 
-import com.nhaarman.mockitokotlin2.*
+import com.nhaarman.mockitokotlin2.argumentCaptor
+import com.nhaarman.mockitokotlin2.times
+import com.nhaarman.mockitokotlin2.verify
 import com.wutsi.blog.app.page.SeleniumTestSupport
 import com.wutsi.blog.app.util.PageName
 import com.wutsi.blog.country.dto.Country
@@ -9,7 +11,6 @@ import com.wutsi.blog.transaction.dto.CreateWalletCommand
 import com.wutsi.blog.transaction.dto.CreateWalletResponse
 import com.wutsi.blog.user.dto.UpdateUserAttributeCommand
 import org.junit.jupiter.api.Test
-import java.util.*
 import kotlin.test.assertEquals
 
 internal class SettingsControllerTest : SeleniumTestSupport() {
