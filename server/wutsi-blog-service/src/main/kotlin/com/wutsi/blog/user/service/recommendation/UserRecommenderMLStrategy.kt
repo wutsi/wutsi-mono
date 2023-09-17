7537ae7d3d9ebec6b8321b3eae615278a3a76b44
wutsi <herve.tchepannou@wutsi.com>
2023-09-17 07:42:56 -0400

    improve the user recommendation algo

--- server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/user/service/recommendation/UserRecommenderMLStrategy.kt
+++ server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/user/service/recommendation/UserRecommenderMLStrategy.kt
@@ -10,6 +10,7 @@ import com.wutsi.blog.user.dto.RecommendUserRequest
 import com.wutsi.blog.user.service.UserRecommenderStrategy
 import com.wutsi.ml.similarity.dto.SearchSimilarityRequest
 import com.wutsi.ml.similarity.dto.SimilarityModelType
+import com.wutsi.platform.core.logging.KVLogger
 import org.springframework.stereotype.Service
 
 @Service
@@ -18,42 +19,58 @@ class UserRecommenderMLStrategy(
     private val similarityBackend: SimilarityBackend,
     private val readerService: ReaderService,
     private val storyService: StoryService,
+    private val logger: KVLogger,
 ) : UserRecommenderStrategy {
     override fun recommend(request: RecommendUserRequest): List<Long> {
-        // get the story ids
-        var userIds = if (request.readerId != null) {
+        // get subscribed blog
+        val userIds: MutableList<Long> = if (request.readerId != null) {
             subscriptionService.search(
                 SearchSubscriptionRequest(
                     subscriberId = request.readerId!!,
                     limit = request.limit,
                 ),
-            ).mapNotNull { it.userId }
+            ).map { it.userId }.toMutableList()
         } else {
-            emptyList()
+            emptyList<Long>().toMutableList()
         }
+        logger.add("blog_subscribed_id", userIds)
 
-        if (userIds.isEmpty()) {
+        // Add recently read blogs
+        if (userIds.size < request.limit) {
             val storyIds = readerService.findViewedStoryIds(request.readerId, request.deviceId)
             if (storyIds.isEmpty()) {
                 return emptyList()
             }
 
             // get the story authors
-            userIds = storyService.searchStories(
+            val readIds = storyService.searchStories(
                 SearchStoryRequest(
                     storyIds = storyIds,
                     limit = storyIds.size,
                 ),
             ).map { it.userId }
+                .filter { !userIds.contains(it) }
+                .toSet()
+            logger.add("blog_read_id", readIds)
+
+            userIds.addAll(readIds)
         }
 
-        // Return similar authors
-        return similarityBackend.search(
-            SearchSimilarityRequest(
-                itemIds = userIds,
-                limit = request.limit,
-                model = SimilarityModelType.AUTHOR_TIFDF,
-            ),
-        ).items.map { it.id }
+        // Add similar users
+        if (userIds.size < request.limit) {
+            val similarIds = similarityBackend.search(
+                SearchSimilarityRequest(
+                    itemIds = userIds.toList(),
+                    limit = request.limit,
+                    model = SimilarityModelType.AUTHOR_TIFDF,
+                ),
+            ).items.map { it.id }
+                .filter { !userIds.contains(it) }
+
+            logger.add("blog_similar_id", similarIds)
+
+            userIds.addAll(similarIds)
+        }
+        return userIds.take(request.limit)
     }
 }
--- server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/user/service/recommendation/UserRecommenderMLStrategyTest.kt
+++ server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/user/service/recommendation/UserRecommenderMLStrategyTest.kt
@@ -19,10 +19,10 @@ import com.wutsi.ml.similarity.dto.Item
 import com.wutsi.ml.similarity.dto.SearchSimilarityRequest
 import com.wutsi.ml.similarity.dto.SearchSimilarityResponse
 import com.wutsi.ml.similarity.dto.SimilarityModelType
+import com.wutsi.platform.core.logging.DefaultKVLogger
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import kotlin.test.assertEquals
-import kotlin.test.assertTrue
 
 class UserRecommenderMLStrategyTest {
     private lateinit var subscriptionService: SubscriptionService
@@ -31,77 +31,118 @@ class UserRecommenderMLStrategyTest {
     private lateinit var storyService: StoryService
     private lateinit var strategy: UserRecommenderMLStrategy
 
+    private val deviceId = "1203920934"
+
     @BeforeEach
     fun setUp() {
         subscriptionService = mock()
         similarityBackend = mock()
         readerService = mock()
         storyService = mock()
-        strategy = UserRecommenderMLStrategy(subscriptionService, similarityBackend, readerService, storyService)
+        strategy = UserRecommenderMLStrategy(
+            subscriptionService,
+            similarityBackend,
+            readerService,
+            storyService,
+            DefaultKVLogger(),
+        )
     }
 
     @Test
-    fun `recommend to user with subscriptions`() {
+    fun `recommend from subscriptions`() {
         // GIVEN
         doReturn(
             listOf(
                 SubscriptionEntity(userId = 1L, subscriberId = 100L),
                 SubscriptionEntity(userId = 2L, subscriberId = 100L),
                 SubscriptionEntity(userId = 3L, subscriberId = 100L),
+                SubscriptionEntity(userId = 5L, subscriberId = 100L),
+                SubscriptionEntity(userId = 6L, subscriberId = 100L),
             ),
         ).whenever(subscriptionService).search(any())
-
-        doReturn(
-            SearchSimilarityResponse(
-                listOf(
-                    Item(10L),
-                    Item(20L),
-                    Item(30L),
-                    Item(40L),
-                ),
-            ),
-        ).whenever(similarityBackend).search(any())
 
         // WHEN
         val result = strategy.recommend(
             RecommendUserRequest(
                 readerId = 100L,
-                deviceId = "1203920934",
-                limit = 10,
+                deviceId = deviceId,
+                limit = 3,
             ),
         )
 
         // THEN
-        assertEquals(listOf(10L, 20L, 30L, 40L), result)
+        assertEquals(listOf(1L, 2L, 3L), result)
 
-        val req = argumentCaptor<SearchSimilarityRequest>()
-        verify(similarityBackend).search(req.capture())
-        assertEquals(listOf(1L, 2L, 3L), req.firstValue.itemIds)
-        assertEquals(SimilarityModelType.AUTHOR_TIFDF, req.firstValue.model)
-        assertEquals(10, req.firstValue.limit)
+        verify(readerService, never()).findViewedStoryIds(anyOrNull(), any())
+        verify(similarityBackend, never()).search(any())
     }
 
     @Test
-    fun `recommend to user without subscriptions`() {
+    fun `recommend from subscriptions and readed`() {
         // GIVEN
-        doReturn(listOf<Long>()).whenever(subscriptionService).search(any())
+        doReturn(
+            listOf(
+                SubscriptionEntity(userId = 1L, subscriberId = 100L),
+            ),
+        ).whenever(subscriptionService).search(any())
 
         doReturn(
-            listOf(11L, 21L, 22L),
+            listOf(10L, 20L, 30L, 40L, 50L, 60L),
         ).whenever(readerService).findViewedStoryIds(anyOrNull(), any())
 
         doReturn(
             listOf(
-                StoryEntity(id = 11L, userId = 1L),
-                StoryEntity(id = 21L, userId = 2L),
-                StoryEntity(id = 22L, userId = 3L),
+                StoryEntity(id = 10L, userId = 1L),
+                StoryEntity(id = 20L, userId = 2L),
+                StoryEntity(id = 30L, userId = 2L),
+                StoryEntity(id = 40L, userId = 2L),
+                StoryEntity(id = 50L, userId = 3L),
+                StoryEntity(id = 60L, userId = 4L),
+            ),
+        ).whenever(storyService).searchStories(any())
+
+        // WHEN
+        val result = strategy.recommend(
+            RecommendUserRequest(
+                readerId = 100L,
+                deviceId = deviceId,
+                limit = 3,
+            ),
+        )
+
+        // THEN
+        assertEquals(listOf(1L, 2L, 3L), result)
+
+        verify(readerService).findViewedStoryIds(100L, deviceId)
+        verify(similarityBackend, never()).search(any())
+    }
+
+    @Test
+    fun `recommend from subscriptions and readed and similar`() {
+        // GIVEN
+        doReturn(
+            listOf(
+                SubscriptionEntity(userId = 1L, subscriberId = 100L),
+            ),
+        ).whenever(subscriptionService).search(any())
+
+        doReturn(
+            listOf(10L, 20L, 30L, 40L, 50L, 60L),
+        ).whenever(readerService).findViewedStoryIds(anyOrNull(), any())
+
+        doReturn(
+            listOf(
+                StoryEntity(id = 10L, userId = 1L),
+                StoryEntity(id = 20L, userId = 2L),
+                StoryEntity(id = 30L, userId = 2L),
+                StoryEntity(id = 40L, userId = 2L),
             ),
         ).whenever(storyService).searchStories(any())
 
         doReturn(
             SearchSimilarityResponse(
                 listOf(
-                    Item(10L),
+                    Item(3L),
                     Item(20L),
                     Item(30L),
                     Item(40L),
@@ -113,21 +154,21 @@ class UserRecommenderMLStrategyTest {
         val result = strategy.recommend(
             RecommendUserRequest(
                 readerId = 100L,
-                deviceId = "1203920934",
-                limit = 10,
+                deviceId = deviceId,
+                limit = 3,
             ),
         )
 
         // THEN
-        assertEquals(listOf(10L, 20L, 30L, 40L), result)
+        assertEquals(listOf(1L, 2L, 3L), result)
 
-        verify(readerService).findViewedStoryIds(100L, "1203920934")
+        verify(readerService).findViewedStoryIds(100L, deviceId)
 
         val req = argumentCaptor<SearchSimilarityRequest>()
         verify(similarityBackend).search(req.capture())
-        assertEquals(listOf(1L, 2L, 3L), req.firstValue.itemIds)
+        assertEquals(listOf(1L, 2L), req.firstValue.itemIds)
         assertEquals(SimilarityModelType.AUTHOR_TIFDF, req.firstValue.model)
-        assertEquals(10, req.firstValue.limit)
+        assertEquals(3, req.firstValue.limit)
     }
 
     @Test
@@ -141,14 +182,13 @@ class UserRecommenderMLStrategyTest {
             listOf(
                 StoryEntity(id = 11L, userId = 1L),
                 StoryEntity(id = 21L, userId = 2L),
-                StoryEntity(id = 22L, userId = 3L),
             ),
         ).whenever(storyService).searchStories(any())
 
         doReturn(
             SearchSimilarityResponse(
                 listOf(
-                    Item(10L),
+                    Item(3L),
                     Item(20L),
                     Item(30L),
                     Item(40L),
@@ -160,42 +200,20 @@ class UserRecommenderMLStrategyTest {
         val result = strategy.recommend(
             RecommendUserRequest(
                 readerId = null,
-                deviceId = "1203920934",
-                limit = 10,
+                deviceId = deviceId,
+                limit = 3,
             ),
         )
 
         // THEN
-        assertEquals(listOf(10L, 20L, 30L, 40L), result)
+        assertEquals(listOf(1L, 2L, 3L), result)
 
-        verify(readerService).findViewedStoryIds(null, "1203920934")
+        verify(readerService).findViewedStoryIds(null, deviceId)
 
         val req = argumentCaptor<SearchSimilarityRequest>()
         verify(similarityBackend).search(req.capture())
-        assertEquals(listOf(1L, 2L, 3L), req.firstValue.itemIds)
+        assertEquals(listOf(1L, 2L), req.firstValue.itemIds)
         assertEquals(SimilarityModelType.AUTHOR_TIFDF, req.firstValue.model)
-        assertEquals(10, req.firstValue.limit)
-    }
-
-    @Test
-    fun `recommend to first time user`() {
-        // GIVEN
-        doReturn(listOf<Long>()).whenever(subscriptionService).search(any())
-
-        doReturn(listOf<Long>()).whenever(readerService).findViewedStoryIds(anyOrNull(), any())
-
-        // WHEN
-        val result = strategy.recommend(
-            RecommendUserRequest(
-                readerId = 100L,
-                deviceId = "1203920934",
-                limit = 10,
-            ),
-        )
-
-        // THEN
-        assertTrue(result.isEmpty())
-
-        verify(similarityBackend, never()).search(any())
+        assertEquals(3, req.firstValue.limit)
     }
 }
