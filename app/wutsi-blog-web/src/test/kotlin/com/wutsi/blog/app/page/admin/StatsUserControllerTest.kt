c42577e6e8e5e272863aa5cda94a34f436f4644b
wutsi <herve.tchepannou@wutsi.com>
2023-09-27 14:17:56 -0400

    add test

--- app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/admin/StatsUserControllerTest.kt
+++ app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/admin/StatsUserControllerTest.kt
@@ -6,24 +6,35 @@ import com.nhaarman.mockitokotlin2.whenever
 import com.wutsi.blog.app.page.SeleniumTestSupport
 import com.wutsi.blog.app.util.PageName
 import com.wutsi.blog.kpi.dto.KpiType
+import com.wutsi.blog.kpi.dto.SearchStoryKpiRequest
+import com.wutsi.blog.kpi.dto.SearchStoryKpiResponse
 import com.wutsi.blog.kpi.dto.SearchUserKpiRequest
 import com.wutsi.blog.kpi.dto.SearchUserKpiResponse
+import com.wutsi.blog.kpi.dto.StoryKpi
 import com.wutsi.blog.kpi.dto.TrafficSource
 import com.wutsi.blog.kpi.dto.UserKpi
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 
 internal class StatsUserControllerTest : SeleniumTestSupport() {
-    private val kpis = listOf(
+    private val userKpis = listOf(
         UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.EMAIL, year = 2020, month = 1, value = 100),
         UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 2, value = 110),
         UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.LINKEDIN, year = 2020, month = 3, value = 120),
+    )
+
+    private val storyKpis = listOf(
+        StoryKpi(storyId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 1, value = 100),
+        StoryKpi(storyId = 10, year = 2020, month = 2, value = 110),
+        StoryKpi(storyId = 10, type = KpiType.READ, year = 2020, month = 3, value = 120)
     )
 
     @Test
     fun index() {
         // GIVEN
         setupLoggedInUser(100)
-        doReturn(SearchUserKpiResponse(kpis)).whenever(kpiBackend).search(any<SearchUserKpiRequest>())
+        doReturn(SearchUserKpiResponse(userKpis)).whenever(kpiBackend).search(any<SearchUserKpiRequest>())
+        Mockito.doReturn(SearchStoryKpiResponse(storyKpis)).whenever(kpiBackend).search(any<SearchStoryKpiRequest>())
 
         // WHEN
         navigate(url("/me/stats/user"))
