33785b158eb46dc53f0936d0ac2946392541fc8b
wutsi <herve.tchepannou@wutsi.com>
2023-11-02 07:37:58 -0400

    improve stats

--- app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/admin/StatsStoryController.kt
+++ app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/admin/StatsStoryController.kt
@@ -1,7 +1,7 @@
 package com.wutsi.blog.app.page.admin
 
-import com.wutsi.blog.app.AbstractPageController
-import com.wutsi.blog.app.model.BarChartModel
+import com.wutsi.blog.app.model.KpiModel
+import com.wutsi.blog.app.model.ReaderModel
 import com.wutsi.blog.app.service.KpiService
 import com.wutsi.blog.app.service.ReaderService
 import com.wutsi.blog.app.service.RequestContext
@@ -14,24 +14,73 @@ import com.wutsi.blog.story.dto.SearchReaderRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.ResponseBody
-import java.time.LocalDate
 
 @Controller
+@RequestMapping("/me/stats/story")
 class StatsStoryController(
-    private val kpiService: KpiService,
-    private val storyService: StoryService,
-    private val readerService: ReaderService,
+    kpiService: KpiService,
+    storyService: StoryService,
+    readerService: ReaderService,
     requestContext: RequestContext,
-) : AbstractPageController(requestContext) {
+) : AbstractStatsController(kpiService, storyService, readerService, requestContext) {
     override fun pageName() = PageName.STATS_STORY
 
-    @GetMapping("/me/stats/story")
-    fun index(
-        @RequestParam(name = "story-id") id: Long,
-        model: Model,
-    ): String {
+    override fun searchStoryReads(period: String?): List<KpiModel> =
+        kpiService.search(
+            SearchStoryKpiRequest(
+                types = listOf(KpiType.READ),
+                dimension = Dimension.ALL,
+                fromDate = fromDate(period),
+            ),
+        )
+
+    override fun searchReads(period: String?): List<KpiModel> =
+        kpiService.search(
+            SearchStoryKpiRequest(
+                storyIds = listOf(getStoryId()),
+                types = listOf(KpiType.READ),
+                fromDate = fromDate(period),
+            ),
+        )
+
+    override fun searchReadTime(period: String?): List<KpiModel> =
+        kpiService.search(
+            SearchStoryKpiRequest(
+                storyIds = listOf(getStoryId()),
+                types = listOf(KpiType.DURATION),
+                fromDate = fromDate(period),
+            ),
+        )
+
+    override fun searchSubscriptions(period: String?): List<KpiModel> =
+        emptyList()
+
+    override fun searchSources(period: String?): List<KpiModel> =
+        kpiService.search(
+            SearchStoryKpiRequest(
+                storyIds = listOf(getStoryId()),
+                types = listOf(KpiType.READ),
+                dimension = Dimension.SOURCE,
+                fromDate = fromDate(period),
+            ),
+        )
+
+    override fun searchReaders(): List<ReaderModel> =
+        readerService.search(
+            SearchReaderRequest(
+                storyId = getStoryId(),
+                subscribedToUserId = requestContext.currentUser()!!.id,
+                limit = 50,
+            ),
+        )
+
+    private fun getStoryId(): Long =
+        requestContext.request.getParameter("story-id").toLong()
+
+    @GetMapping
+    fun index(@RequestParam(name = "story-id") id: Long, model: Model): String {
         val story = storyService.get(id, withKpis = true)
 
         model.addAttribute("story", story)
@@ -39,76 +88,74 @@ class StatsStoryController(
         return "admin/stats-story"
     }
 
-    @GetMapping("/me/stats/story/chart/read")
-    @ResponseBody
-    fun read(
-        @RequestParam(name = "story-id") storyId: Long,
-    ): BarChartModel =
-        kpiService.toBarChartModel(
-            kpis = kpiService.search(
-                SearchStoryKpiRequest(
-                    storyIds = listOf(storyId),
-                    types = listOf(KpiType.READ),
-                ),
-            ),
-            type = KpiType.READ,
-        )
-
-    @GetMapping("/me/stats/story/chart/read-time")
-    @ResponseBody
-    fun readTime(
-        @RequestParam(name = "story-id") storyId: Long,
-    ): BarChartModel =
-        kpiService.toBarChartModel(
-            kpis = kpiService.search(
-                SearchStoryKpiRequest(
-                    storyIds = listOf(storyId),
-                    types = listOf(KpiType.DURATION),
-                ),
-            ),
-            type = KpiType.DURATION,
-        )
-
-    @GetMapping("/me/stats/story/chart/source")
-    @ResponseBody
-    fun source(
-        @RequestParam(name = "story-id") storyId: Long,
-        @RequestParam(required = false) period: String? = null,
-    ): BarChartModel {
-        val toDate = if (period?.lowercase() == "l30") LocalDate.now() else null
-        val fromDate = toDate?.let { toDate.minusDays(30) }
-        return kpiService.toKpiModelBySource(
-            kpis = kpiService.search(
-                SearchStoryKpiRequest(
-                    storyIds = listOf(storyId),
-                    types = listOf(KpiType.READ),
-                    dimension = Dimension.SOURCE,
-                    fromDate = fromDate,
-                    toDate = toDate
-                ),
-            ),
-            type = KpiType.READ,
-        )
-    }
-
-    @GetMapping("/me/stats/story/readers")
-    fun readers(
-        @RequestParam(name = "story-id") storyId: Long,
-        model: Model,
-    ): String {
-        val user = requestContext.currentUser()
-        if (user != null) {
-            val readers = readerService.search(
-                SearchReaderRequest(
-                    storyId = storyId,
-                    subscribedToUserId = user.id,
-                    limit = 50,
-                ),
-            )
-            if (readers.isNotEmpty()) {
-                model.addAttribute("readers", readers)
-            }
-        }
-        return "admin/fragment/stats-readers"
-    }
+//    @GetMapping("/readers")
+//    fun readers(
+//        @RequestParam(name = "story-id") storyId: Long,
+//        model: Model,
+//    ): String {
+//        val user = requestContext.currentUser()
+//        if (user != null) {
+//            val readers = readerService.search(
+//                SearchReaderRequest(
+//                    storyId = storyId,
+//                    subscribedToUserId = user.id,
+//                    limit = 50,
+//                ),
+//            )
+//            if (readers.isNotEmpty()) {
+//                model.addAttribute("readers", readers)
+//            }
+//        }
+//        return "admin/fragment/stats-readers"
+//    }
+//    @GetMapping("/me/stats/story/chart/read")
+//    @ResponseBody
+//    fun read(
+//        @RequestParam(name = "story-id") storyId: Long,
+//    ): BarChartModel =
+//        kpiService.toBarChartModel(
+//            kpis = kpiService.search(
+//                SearchStoryKpiRequest(
+//                    storyIds = listOf(storyId),
+//                    types = listOf(KpiType.READ),
+//                ),
+//            ),
+//            type = KpiType.READ,
+//        )
+//
+//    @GetMapping("/me/stats/story/chart/read-time")
+//    @ResponseBody
+//    fun readTime(
+//        @RequestParam(name = "story-id") storyId: Long,
+//    ): BarChartModel =
+//        kpiService.toBarChartModel(
+//            kpis = kpiService.search(
+//                SearchStoryKpiRequest(
+//                    storyIds = listOf(storyId),
+//                    types = listOf(KpiType.DURATION),
+//                ),
+//            ),
+//            type = KpiType.DURATION,
+//        )
+//    @GetMapping("/me/stats/story/chart/source")
+//    @ResponseBody
+//    fun source(
+//        @RequestParam(name = "story-id") storyId: Long,
+//        @RequestParam(required = false) period: String? = null,
+//    ): BarChartModel {
+//        val toDate = if (period?.lowercase() == "l30") LocalDate.now() else null
+//        val fromDate = toDate?.let { toDate.minusDays(30) }
+//        return kpiService.toKpiModelBySource(
+//            kpis = kpiService.search(
+//                SearchStoryKpiRequest(
+//                    storyIds = listOf(storyId),
+//                    types = listOf(KpiType.READ),
+//                    dimension = Dimension.SOURCE,
+//                    fromDate = fromDate,
+//                    toDate = toDate
+//                ),
+//            ),
+//            type = KpiType.READ,
+//        )
+//    }
 }
