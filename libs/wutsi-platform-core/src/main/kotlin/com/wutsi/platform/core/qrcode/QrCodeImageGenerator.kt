e5a7d3de5c9a38496d152eed73a9d19aa0de968a
wutsi <herve.tchepannou@wutsi.com>
2023-11-07 09:27:20 -0500

    upgrade

--- libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/qrcode/QrCodeImageGenerator.kt
+++ libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/qrcode/QrCodeImageGenerator.kt
@@ -1,64 +1,28 @@
 package com.wutsi.platform.core.qrcode
 
-import io.github.g0dkar.qrcode.QRCode
-import org.slf4j.LoggerFactory
-import java.awt.Color
-import java.awt.Graphics2D
-import java.awt.image.BufferedImage
+import qrcode.QRCode
 import java.io.ByteArrayInputStream
-import java.io.ByteArrayOutputStream
 import java.io.OutputStream
-import java.net.URL
 import javax.imageio.ImageIO
 
-class QrCodeImageGenerator(
-    private val logoUrl: URL? = null,
-) {
-    companion object {
-        private val LOGGER = LoggerFactory.getLogger(QrCodeImageGenerator::class.java)
-    }
+class QrCodeImageGenerator {
+    fun generate(
+        data: String,
+        output: OutputStream,
+        logo: ByteArray? = null,
+        logoWidth: Int = 300,
+        logoutHeight: Int = 300
+    ) {
+        val builder = QRCode.ofSquares()
+            .withSize(30)
 
-    fun generate(data: String, output: OutputStream) {
-        val qr = ByteArrayOutputStream()
-        QRCode(data)
-            .render(margin = 30, cellSize = 30)
-            .writeImage(qr)
-
-        val logo = downloadLogo()
-        val combined = combine(ImageIO.read(ByteArrayInputStream(qr.toByteArray())), logo)
-        ImageIO.write(combined, "png", output)
-    }
-
-    private fun downloadLogo(): BufferedImage? =
-        try {
-            logoUrl?.let { ImageIO.read(it) }
-        } catch (ex: Exception) {
-            LOGGER.warn("Unable to download image $logoUrl", ex)
-            null
+        if (logo != null) {
+            builder.withLogo(logo, logoWidth, logoutHeight, true)
         }
 
-    private fun combine(qrcode: BufferedImage, logo: BufferedImage?): BufferedImage {
-        logo ?: return qrcode
+        val qr = builder.build(data).render()
 
-        val deltaHeight: Int = qrcode.height - logo.height
-        val deltaWidth: Int = qrcode.width - logo.width
-
-        val combined = BufferedImage(qrcode.width, qrcode.height, BufferedImage.TYPE_INT_ARGB)
-        val g2 = combined.graphics as Graphics2D
-        g2.drawImage(qrcode, 0, 0, null)
-        g2.background = Color.WHITE
-        g2.fillOval(
-            Math.round((deltaWidth / 2).toFloat()),
-            Math.round((deltaHeight / 2).toFloat()),
-            logo.width,
-            logo.height,
-        )
-        g2.drawImage(
-            logo,
-            Math.round((deltaWidth / 2).toFloat()),
-            Math.round((deltaHeight / 2).toFloat()),
-            null,
-        )
-        return combined
+        val img = ImageIO.read(ByteArrayInputStream(qr))
+        ImageIO.write(img, "png", output)
     }
 }
--- libs/wutsi-platform-core/src/test/kotlin/com/wutsi/platform/core/qrcode/QrCodeImageGeneratorTest.kt
+++ libs/wutsi-platform-core/src/test/kotlin/com/wutsi/platform/core/qrcode/QrCodeImageGeneratorTest.kt
@@ -1,7 +1,11 @@
 package com.wutsi.platform.core.qrcode
 
+import org.apache.commons.io.IOUtils
 import org.junit.jupiter.api.Test
+import java.io.ByteArrayInputStream
 import java.io.ByteArrayOutputStream
+import java.io.File
+import java.io.FileOutputStream
 import java.net.URL
 
 internal class QrCodeImageGeneratorTest {
@@ -12,13 +16,32 @@ internal class QrCodeImageGeneratorTest {
         val generator = QrCodeImageGenerator()
         val out = ByteArrayOutputStream()
         generator.generate(data, out)
+
+        toFile("qr.png", out.toByteArray())
     }
 
     @Test
     fun generateWithLogo() {
-        val generator =
-            QrCodeImageGenerator(URL("https://prod-wutsi.s3.amazonaws.com/static/wutsi-blog-web/assets/wutsi/img/logo/name-104x50.png"))
-        val out = ByteArrayOutputStream()
-        generator.generate(data, out)
+        val generator = QrCodeImageGenerator()
+        val logoStream =
+            URL("https://prod-wutsi.s3.amazonaws.com/static/wutsi-blog-web/assets/wutsi/img/logo/name-104x50.png").openStream()
+        logoStream.use {
+            val logo = ByteArrayOutputStream()
+            IOUtils.copy(logoStream, logo)
+            val out = ByteArrayOutputStream()
+            generator.generate(data, out, logo.toByteArray(), 300, 300)
+
+            toFile("qr-logo.png", out.toByteArray())
+        }
+    }
+
+    private fun toFile(name: String, out: ByteArray) {
+        val file = File(System.getProperty("user.home") + "/wutsi", name)
+        file.parentFile.mkdirs()
+
+        val fout = FileOutputStream(file)
+        fout.use {
+            IOUtils.copy(ByteArrayInputStream(out), fout)
+        }
     }
 }
