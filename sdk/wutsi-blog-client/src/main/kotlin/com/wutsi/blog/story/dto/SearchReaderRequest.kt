0d6254b228f8da6912d58d81898db793e910091b
wutsi <herve.tchepannou@wutsi.com>
2023-11-09 08:20:57 -0500

    filter email readers

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/SearchReaderRequest.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/SearchReaderRequest.kt
@@ -3,6 +3,7 @@ package com.wutsi.blog.story.dto
 data class SearchReaderRequest(
     val storyId: Long? = null,
     val subscribedToUserId: Long? = null,
+    val email: Boolean? = null,
     val limit: Int = 20,
     val offset: Int = 0,
 )
