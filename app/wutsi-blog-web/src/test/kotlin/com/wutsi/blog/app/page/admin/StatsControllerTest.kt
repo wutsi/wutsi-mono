5dfcd768b09dc49a8246b7ae56ec159f5715ec05
wutsi <herve.tchepannou@wutsi.com>
2023-11-19 15:58:20 -0500

    improve stats

--- app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/admin/StatsControllerTest.kt
+++ app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/admin/StatsControllerTest.kt
@@ -16,33 +16,33 @@ import org.junit.jupiter.api.Test
 import org.mockito.Mockito.doReturn
 
 internal class StatsControllerTest : SeleniumTestSupport() {
-    private val userKpis = listOf(
-        UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.EMAIL, year = 2020, month = 1, value = 100),
-        UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 2, value = 110),
-        UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.LINKEDIN, year = 2020, month = 3, value = 120),
-    )
+	private val userKpis = listOf(
+		UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.EMAIL, year = 2020, month = 1, value = 100),
+		UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 2, value = 110),
+		UserKpi(userId = 10, type = KpiType.READ, source = TrafficSource.LINKEDIN, year = 2020, month = 3, value = 120),
+	)
 
-    private val storyKpis = listOf(
-        StoryKpi(storyId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 1, value = 100),
-        StoryKpi(storyId = 10, year = 2020, month = 2, value = 110),
-        StoryKpi(storyId = 10, type = KpiType.READ, year = 2020, month = 3, value = 120),
-    )
+	private val storyKpis = listOf(
+		StoryKpi(storyId = 10, type = KpiType.READ, source = TrafficSource.DIRECT, year = 2020, month = 1, value = 100),
+		StoryKpi(storyId = 10, year = 2020, month = 2, value = 110),
+		StoryKpi(storyId = 10, type = KpiType.READ, year = 2020, month = 3, value = 120),
+	)
 
-    @Test
-    fun index() {
-        // GIVEN
-        setupLoggedInUser(100, superUser = true)
-        doReturn(SearchUserKpiResponse(userKpis)).whenever(kpiBackend).search(any<SearchUserKpiRequest>())
-        doReturn(SearchStoryKpiResponse(storyKpis)).whenever(kpiBackend).search(any<SearchStoryKpiRequest>())
+	@Test
+	fun index() {
+		// GIVEN
+		setupLoggedInUser(100, superUser = true)
+		doReturn(SearchUserKpiResponse(userKpis)).whenever(kpiBackend).search(any<SearchUserKpiRequest>())
+		doReturn(SearchStoryKpiResponse(storyKpis)).whenever(kpiBackend).search(any<SearchStoryKpiRequest>())
 
-        // WHEN
-        navigate(url("/me/stats"))
+		// WHEN
+		navigate(url("/me/stats"))
 
-        // THEN
-        assertCurrentPageIs(PageName.STATS)
+		// THEN
+		assertCurrentPageIs(PageName.STATS)
 
-        assertElementPresent("#chart-area-read")
-        assertElementPresent("#chart-area-read-time")
-        assertElementPresent("#chart-area-source")
-    }
+		assertElementPresent("#chart-area-read")
+		assertElementPresent("#chart-area-read-time")
+		assertElementPresent("#table-source")
+	}
 }
