1d0f96a1b52b3c4b43897a3fa0539a765aa7438e
wutsi <herve.tchepannou@wutsi.com>
2023-11-22 00:37:38 -0500

    fix test

--- app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/create/CreateControllerTest.kt
+++ app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/create/CreateControllerTest.kt
@@ -102,8 +102,8 @@ class CreateControllerTest : SeleniumTestSupport() {
 
         // Review
         assertCurrentPageIs(PageName.CREATE_REVIEW)
-        click("#writer-id-${users[0].id}")
-        click("#writer-id-${users[1].id}")
+        click("#chk-writer-${users[0].id}")
+        click("#chk-writer-${users[1].id}")
         assertElementCount("#writer-container .author-suggestion-card", users.size)
         click("#btn-create")
 
