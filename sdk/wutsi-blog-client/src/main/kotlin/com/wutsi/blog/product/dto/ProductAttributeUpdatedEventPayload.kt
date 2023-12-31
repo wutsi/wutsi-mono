0656863452e2e566401572a01f1cd2fb07e2ad94
wutsi <herve.tchepannou@wutsi.com>
2023-12-30 22:10:18 -0500

    remove unused schema

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/ProductAttributeUpdatedEventPayload.kt
+++
@@ -1,6 +0,0 @@
-package com.wutsi.blog.product.dto
-
-data class ProductAttributeUpdatedEventPayload(
-    val name: String = "",
-    val value: String? = null,
-)
--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/UpdateProductAttributeCommand.kt
+++
@@ -1,9 +0,0 @@
-package com.wutsi.blog.product.dto
-
-import javax.validation.constraints.NotEmpty
-
-data class UpdateProductAttributeCommand(
-    val productId: Long = -1,
-    @get:NotEmpty val name: String = "",
-    val value: String = "",
-)
