94047b4d9755ebed5bb32e58a86e5f41bed0c110
wutsi <herve.tchepannou@wutsi.com>
2024-01-10 13:19:32 -0500

    add book schema

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/ChangeBookLocationCommand.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/ChangeBookLocationCommand.kt
@@ -3,4 +3,6 @@ package com.wutsi.blog.product.dto
 data class ChangeBookLocationCommand(
     val bookId: Long = 1,
     val location: String = "",
+    val readPercentage: Int = 0,
+    val readed: Boolean = false,
 )
