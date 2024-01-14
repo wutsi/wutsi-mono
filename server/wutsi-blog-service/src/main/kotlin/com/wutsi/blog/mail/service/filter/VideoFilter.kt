80df29e5b7ffc9d915a57943f1be1ec60e48d765
wutsi <herve.tchepannou@wutsi.com>
2024-01-13 22:37:32 -0500

    add video player overlay

--- server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/mail/service/filter/VideoFilter.kt
+++ server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/mail/service/filter/VideoFilter.kt
@@ -7,10 +7,14 @@ import org.jsoup.Jsoup
 class VideoFilter(private val assetUrl: String) : MailFilter {
     override fun filter(html: String, context: MailContext): String {
         val doc = Jsoup.parse(html)
+        doc.select(".player").forEach {
+            it.addClass("border")
+        }
         doc.select(".play-icon").forEach {
             it.html(
-                "<img width=\"32\" src=\"$assetUrl/assets/wutsi/img/mail/play-video.png\" style=\"vertical-align: middle\"/>",
+                "<img width=\"32\" src=\"$assetUrl/assets/wutsi/img/play.png\" style=\"vertical-align: middle\"/>",
             )
+            it.parent()?.addClass("border-top")
         }
         return doc.html()
     }
