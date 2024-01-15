56a6d5dffbcbc6ce72deb49cc843e8a6b8751f08
wutsi <herve.tchepannou@wutsi.com>
2024-01-14 19:43:24 -0500

    fix lint issues

--- server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/service/discount/DonationDiscountRuleTest.kt
+++ server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/service/discount/DonationDiscountRuleTest.kt
@@ -158,7 +158,6 @@ class DonationDiscountRuleTest {
 
         // THEN
         assertNull(discount)
-
     }
 
     private fun createTransaction(amount: Long, date: Date = Date()) = TransactionEntity(
