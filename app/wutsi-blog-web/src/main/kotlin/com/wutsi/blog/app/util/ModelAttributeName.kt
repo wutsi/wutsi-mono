4992b0483ee22354188568d84712fe25bfffa88d
wutsi <herve.tchepannou@wutsi.com>
2024-03-26 15:25:58 -0400

    minify

--- app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/util/ModelAttributeName.kt
+++ app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/util/ModelAttributeName.kt
@@ -8,6 +8,5 @@ class ModelAttributeName {
         const val PAGE = "page"
         const val HITID = "hitId"
         const val REQUEST_CONTEXT = "requestContext"
-        const val ADS_BANNER = "adsBanner"
     }
 }
