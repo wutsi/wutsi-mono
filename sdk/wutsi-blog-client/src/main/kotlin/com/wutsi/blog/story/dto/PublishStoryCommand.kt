30fd4b7cfb2177457f6b5f56c7a49d591fbd2416
wutsi <herve.tchepannou@wutsi.com>
2024-05-19 19:55:23 -0400

    remove summary and tags

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/PublishStoryCommand.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/PublishStoryCommand.kt
@@ -5,10 +5,7 @@ import java.util.Date
 data class PublishStoryCommand(
     val storyId: Long = -1,
     val title: String? = null,
-    val summary: String? = null,
-    val topicId: Long? = null,
     val categoryId: Long? = null,
-    val tags: List<String>? = null,
     val tagline: String? = null,
     val access: StoryAccess? = null,
     val timestamp: Long = System.currentTimeMillis(),
--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/StoryPublishedEventPayload.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/story/dto/StoryPublishedEventPayload.kt
@@ -2,9 +2,6 @@ package com.wutsi.blog.story.dto
 
 data class StoryPublishedEventPayload(
     val title: String? = null,
-    val summary: String? = null,
-    val topicId: Long? = null,
-    val tags: List<String>? = null,
     val tagline: String? = null,
     val access: StoryAccess? = null,
     val categoryId: Long? = null,
