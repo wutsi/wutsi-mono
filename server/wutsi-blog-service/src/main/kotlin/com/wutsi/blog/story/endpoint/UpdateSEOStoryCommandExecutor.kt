856898cbce01b6723803bc8c104b33856ba61e52
wutsi <herve.tchepannou@wutsi.com>
2024-05-21 17:53:45 -0400

    update SEO by storyId

--- server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/story/endpoint/UpdateSEOStoryCommandExecutor.kt
+++ server/wutsi-blog-service/src/main/kotlin/com/wutsi/blog/story/endpoint/UpdateSEOStoryCommandExecutor.kt
@@ -1,10 +1,12 @@
 package com.wutsi.blog.story.endpoint
 
 import com.wutsi.blog.SortOrder
+import com.wutsi.blog.story.domain.StoryEntity
 import com.wutsi.blog.story.dto.SearchStoryRequest
 import com.wutsi.blog.story.dto.StorySortStrategy
 import com.wutsi.blog.story.dto.StoryStatus
 import com.wutsi.blog.story.service.StoryService
+import javassist.NotFoundException
 import org.slf4j.LoggerFactory
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,7 +23,15 @@ class UpdateSEOStoryCommandExecutor(
     }
 
     @GetMapping
-    fun create(@RequestParam(name = "user-id") userId: Long) {
+    fun create(
+        @RequestParam(name = "user-id", required = false) userId: Long? = null,
+        @RequestParam(name = "story-id", required = false) storyId: Long? = null,
+    ) {
+        storyId?.let { id -> updateByStory(id) }
+        userId?.let { id -> updateByUserId(id) }
+    }
+
+    private fun updateByUserId(userId: Long) {
         var offset = 0
         var i = 0
         while (true) {
@@ -40,17 +50,29 @@ class UpdateSEOStoryCommandExecutor(
             }
 
             stories.forEach { story ->
-                try {
-                    LOGGER.info(">>> $i. Updating SEO information of Story#${story.id} - ${story.title}")
-                    service.updateSEOInformation(story)
-                    Thread.sleep(4000) // Pause to respect the free quota: 15 request per minute
-                } catch (ex: Exception) {
-                    LOGGER.warn("Unexpected error", ex)
-                } finally {
-                    i++
-                }
+                update(i++, story)
             }
             offset += stories.size
+        }
+    }
+
+    private fun updateByStory(storyId: Long) {
+        try {
+            val story = service.findById(storyId)
+            update(0, story)
+        } catch (ex: NotFoundException) {
+            LOGGER.warn("Story not found", ex)
+        }
+    }
+
+    private fun update(i: Int, story: StoryEntity) {
+        try {
+            LOGGER.info(">>> $i. Updating SEO information of Story#${story.id} - ${story.title}")
+            service.updateSEOInformation(story)
+            Thread.sleep(4000) // Pause to respect the free quota: 15 request per minute
+        } catch (ex: Exception) {
+            LOGGER.warn("Unexpected error", ex)
+        } finally {
         }
     }
 }
