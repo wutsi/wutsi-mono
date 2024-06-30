adc1f984879ab64322df42cf8aff2faed37f3d5e
wutsi <herve.tchepannou@wutsi.com>
2024-06-30 15:45:59 -0400

    fix test

--- server/wutsi-blog-service/src/main/resources/db/migration/base/ZZ2_085__category-counter.sql
+++ server/wutsi-blog-service/src/main/resources/db/migration/base/ZZ2_085__category-counter.sql
@@ -9,33 +9,3 @@ UPDATE T_STORY set category_fk=2331 WHERE category_fk=1221;
 UPDATE T_STORY set category_fk=2327 WHERE category_fk=1222;
 DELETE FROM T_CATEGORY where parent_fk=1200;
 DELETE FROM T_CATEGORY where id=1200;
-
-DROP TABLE IF EXISTS TMP_STORY_CATEGORY;
-CREATE TEMPORARY TABLE TMP_STORY_CATEGORY AS
-    SELECT
-        C.parent_fk as parent_id,
-        C.long_title as long_title,
-        S.category_fk as category_id,
-        count(*) as count
-    FROM
-        T_STORY S JOIN T_USER U on S.user_fk=U.id
-        JOIN T_CATEGORY C on S.category_fk=C.id
-    WHERE S.status=1 AND U.suspended=false AND category_fk IS NOT NULL
-    GROUP BY category_fk;
-
-ALTER TABLE T_CATEGORY ADD COLUMN story_count BIGINT NOT NULL DEFAULT 0;
-UPDATE T_CATEGORY C, TMP_STORY_CATEGORY T SET C.story_count=T.count WHERE C.id=T.category_id;
-
-DROP TABLE IF EXISTS TMP_STORY_PARENT_CATEGORY;
-CREATE TEMPORARY TABLE TMP_STORY_PARENT_CATEGORY AS
-    SELECT
-        parent_id as category_id,
-        sum(count) count
-    FROM TMP_STORY_CATEGORY
-    GROUP BY parent_id;
-
-UPDATE T_CATEGORY C, TMP_STORY_PARENT_CATEGORY T
-    SET C.story_count=C.story_count+T.count
-    WHERE C.id=T.category_id;
-
-
