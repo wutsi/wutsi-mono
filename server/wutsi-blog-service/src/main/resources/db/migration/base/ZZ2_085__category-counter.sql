f394ca0806ef8c6bbbf16b36382cd15c45630afa
wutsi <herve.tchepannou@wutsi.com>
2024-06-30 16:32:16 -0400

    add comics category

--- server/wutsi-blog-service/src/main/resources/db/migration/base/ZZ2_085__category-counter.sql
+++ server/wutsi-blog-service/src/main/resources/db/migration/base/ZZ2_085__category-counter.sql
@@ -1,3 +1,4 @@
+-- Professional resources -> Business
 UPDATE T_STORY set category_fk=2314 WHERE category_fk=1212;
 UPDATE T_STORY set category_fk=2310 WHERE category_fk=1213;
 UPDATE T_STORY set category_fk=2328 WHERE category_fk=1214;
@@ -7,5 +8,21 @@ UPDATE T_STORY set category_fk=2317 WHERE category_fk=1218;
 UPDATE T_STORY set category_fk=2330 WHERE category_fk=1220;
 UPDATE T_STORY set category_fk=2331 WHERE category_fk=1221;
 UPDATE T_STORY set category_fk=2327 WHERE category_fk=1222;
+UPDATE T_PRODUCT set category_fk=2314 WHERE category_fk=1212;
+UPDATE T_PRODUCT set category_fk=2310 WHERE category_fk=1213;
+UPDATE T_PRODUCT set category_fk=2328 WHERE category_fk=1214;
+UPDATE T_PRODUCT set category_fk=2329 WHERE category_fk=1215;
+UPDATE T_PRODUCT set category_fk=2313 WHERE category_fk=1216;
+UPDATE T_PRODUCT set category_fk=2317 WHERE category_fk=1218;
+UPDATE T_PRODUCT set category_fk=2330 WHERE category_fk=1220;
+UPDATE T_PRODUCT set category_fk=2331 WHERE category_fk=1221;
+UPDATE T_PRODUCT set category_fk=2327 WHERE category_fk=1222;
+
 DELETE FROM T_CATEGORY where parent_fk=1200;
 DELETE FROM T_CATEGORY where id=1200;
+
+-- Literature > Manga -> Comics > Manga
+UPDATE T_STORY set category_fk=2427 WHERE category_fk=1124;
+UPDATE T_PRODUCT set category_fk=2427 WHERE category_fk=1124;
+
+DELETE FROM T_CATEGORY where id=1124;
