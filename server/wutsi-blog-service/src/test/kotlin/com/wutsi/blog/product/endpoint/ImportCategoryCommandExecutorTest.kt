0b8c725ea1ab079ed676d7bea31dce65b1b0277d
wutsi <herve.tchepannou@wutsi.com>
2024-06-30 16:13:18 -0400

    fix test

--- server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/endpoint/ImportCategoryCommandExecutorTest.kt
+++ server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/endpoint/ImportCategoryCommandExecutorTest.kt
@@ -31,9 +31,9 @@ class ImportCategoryCommandExecutorTest {
         assertEquals(1, category.level)
         assertEquals(1600L, category.parent?.id)
         assertEquals("Animals", category.title)
-        assertEquals("Leisure and hobbies > Animals", category.longTitle)
+        assertEquals("Hobbies > Animals", category.longTitle)
         assertEquals("Animaux", category.titleFrench)
-        assertEquals("Loisir et hobbies > Animaux", category.longTitleFrench)
+        assertEquals("Loisirs > Animaux", category.longTitleFrench)
         assertEquals("Animaux", category.titleFrenchAscii)
     }
 }
