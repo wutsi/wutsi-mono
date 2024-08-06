c04431c126a68adb7f8bf389bf85782782278e72
wutsi <herve.tchepannou@wutsi.com>
2024-08-05 20:42:43 -0400

    change schema

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/transaction/dto/CaptureTransactionCommand.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/transaction/dto/CaptureTransactionCommand.kt
@@ -3,6 +3,6 @@ package com.wutsi.blog.transaction.dto
 import jakarta.validation.constraints.NotEmpty
 
 data class CaptureTransactionCommand(
-    @NotEmpty val transactionId: String = "",
+    @NotEmpty val gatewayTransactionId: String = "",
     val timestamp: Long = System.currentTimeMillis(),
 )
