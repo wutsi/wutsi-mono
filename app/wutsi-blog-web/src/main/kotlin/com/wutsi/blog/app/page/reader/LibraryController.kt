e1d25711a0314fdecf62842b1de9d1318b198f97
wutsi <herve.tchepannou@wutsi.com>
2024-08-07 21:11:39 -0400

    improve product recommendation

--- app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/reader/LibraryController.kt
+++ app/wutsi-blog-web/src/main/kotlin/com/wutsi/blog/app/page/reader/LibraryController.kt
@@ -6,20 +6,25 @@ import com.wutsi.blog.app.page.AbstractPageController
 import com.wutsi.blog.app.service.BookService
 import com.wutsi.blog.app.service.ProductService
 import com.wutsi.blog.app.service.RequestContext
+import com.wutsi.blog.app.service.UserService
 import com.wutsi.blog.app.util.PageName
 import com.wutsi.blog.product.dto.ProductSortStrategy
 import com.wutsi.blog.product.dto.ProductStatus
+import com.wutsi.blog.product.dto.ProductType
 import com.wutsi.blog.product.dto.SearchBookRequest
 import com.wutsi.blog.product.dto.SearchProductContext
 import com.wutsi.blog.product.dto.SearchProductRequest
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
 
 @Controller
 class LibraryController(
     private val bookService: BookService,
     private val productService: ProductService,
+    private val userService: UserService,
+
     requestContext: RequestContext,
 ) : AbstractPageController(requestContext) {
     override fun pageName() = PageName.LIBRARY
@@ -27,8 +32,43 @@ class LibraryController(
     @GetMapping("/me/library")
     fun index(model: Model): String {
         val books = loadBooks(model)
-        loadProducts(books, model)
+
+        val storeIds = books.map { book -> book.product.storeId }.toSet().toList()
+        model.addAttribute("storeIds", storeIds)
+
+        loadProducts(storeIds, model)
+
         return "reader/library"
+    }
+
+    @GetMapping("/me/library/stores/{store-id}")
+    fun store(@PathVariable(name = "store-id") storeId: String, model: Model): String {
+        val store = storeService.get(storeId)
+        val user = userService.get(store.userId)
+
+        val products = productService.search(
+            request = SearchProductRequest(
+                status = ProductStatus.PUBLISHED,
+                types = listOf(ProductType.COMICS, ProductType.EBOOK),
+                storeIds = listOf(storeId),
+                available = true,
+                limit = 4,
+                sortBy = ProductSortStrategy.PUBLISHED,
+                excludePurchasedProduct = true,
+                dedupUser = true,
+                searchContext = SearchProductContext(
+                    userId = requestContext.currentUser()?.id,
+                )
+            ),
+        )
+
+        if (products.isNotEmpty()) {
+            model.addAttribute("products", products)
+            model.addAttribute("merchant", user)
+            model.addAttribute("store", store)
+        }
+
+        return "reader/fragment/library-store"
     }
 
     fun loadBooks(model: Model): List<BookModel> {
@@ -45,21 +85,21 @@ class LibraryController(
         return books
     }
 
-    fun loadProducts(books: List<BookModel>, model: Model): List<ProductModel> {
+    fun loadProducts(storeIds: List<String>, model: Model): List<ProductModel> {
         val products = productService.search(
             request = SearchProductRequest(
                 status = ProductStatus.PUBLISHED,
-                excludeProductIds = books.map { book -> book.product.id },
+                types = listOf(ProductType.COMICS, ProductType.EBOOK),
                 available = true,
-                limit = 20,
-                sortBy = ProductSortStrategy.PUBLISHED,
-                bubbleDownPurchasedProduct = true,
+                limit = 100,
+                sortBy = ProductSortStrategy.RECOMMENDED,
+                excludePurchasedProduct = true,
                 dedupUser = true,
                 searchContext = SearchProductContext(
                     userId = requestContext.currentUser()?.id,
                 )
             ),
-        )
+        ).filter { product -> !storeIds.contains(product.storeId) }
 
         if (products.isNotEmpty()) {
             model.addAttribute("products", products)
--- app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/reader/LibraryControllerTest.kt
+++ app/wutsi-blog-web/src/test/kotlin/com/wutsi/blog/app/page/reader/LibraryControllerTest.kt
@@ -7,9 +7,18 @@ import com.wutsi.blog.app.page.SeleniumTestSupport
 import com.wutsi.blog.app.page.store.ShopControllerTest
 import com.wutsi.blog.app.util.PageName
 import com.wutsi.blog.product.dto.BookSummary
+import com.wutsi.blog.product.dto.Discount
+import com.wutsi.blog.product.dto.DiscountType
+import com.wutsi.blog.product.dto.GetStoreResponse
+import com.wutsi.blog.product.dto.Offer
 import com.wutsi.blog.product.dto.ProductStatus
 import com.wutsi.blog.product.dto.ProductSummary
 import com.wutsi.blog.product.dto.SearchBookResponse
+import com.wutsi.blog.product.dto.SearchOfferResponse
+import com.wutsi.blog.product.dto.SearchProductResponse
+import com.wutsi.blog.product.dto.Store
+import com.wutsi.blog.user.dto.GetUserResponse
+import com.wutsi.blog.user.dto.User
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import java.util.Date
@@ -18,7 +27,60 @@ class LibraryControllerTest : SeleniumTestSupport() {
     companion object {
         const val BLOG_ID = 1L
         const val USER_ID = 555L
+        const val STORE_ID = "100"
     }
+
+    private val products = listOf(
+        ProductSummary(
+            id = 100,
+            title = "Product 100",
+            imageUrl = "https://picsum.photos/1200/600",
+            fileUrl = "https://www.google.ca/123.pdf",
+            storeId = ShopControllerTest.STORE_ID,
+            price = 1000,
+            currency = "XAF",
+            status = ProductStatus.PUBLISHED,
+            available = true,
+            slug = "/product/100/product-100",
+        ),
+        ProductSummary(
+            id = 200,
+            title = "Product 200",
+            imageUrl = "https://picsum.photos/1200/600",
+            fileUrl = "https://www.google.ca/123.pdf",
+            storeId = ShopControllerTest.STORE_ID,
+            price = 1500,
+            currency = "XAF",
+            status = ProductStatus.PUBLISHED,
+            available = true,
+            slug = "/product/200/product-200",
+        )
+    )
+
+    private val offers = listOf(
+        Offer(
+            productId = 100,
+            price = 800,
+            referencePrice = 1000,
+            savingAmount = 200,
+            savingPercentage = 20,
+            discount = Discount(
+                type = DiscountType.SUBSCRIBER,
+                percentage = 20
+            )
+        ),
+        Offer(
+            productId = 200,
+            price = 1200,
+            referencePrice = 1500,
+            savingAmount = 300,
+            savingPercentage = 20,
+            discount = Discount(
+                type = DiscountType.SUBSCRIBER,
+                percentage = 20
+            )
+        )
+    )
 
     private val books = listOf(
         BookSummary(
@@ -29,7 +91,7 @@ class LibraryControllerTest : SeleniumTestSupport() {
                 title = "Product 100",
                 imageUrl = "https://picsum.photos/1200/600",
                 fileUrl = "https://www.google.ca/123.pdf",
-                storeId = ShopControllerTest.STORE_ID,
+                storeId = STORE_ID,
                 price = 1000,
                 currency = "XAF",
                 status = ProductStatus.PUBLISHED,
@@ -45,7 +107,7 @@ class LibraryControllerTest : SeleniumTestSupport() {
                 title = "Product 200",
                 imageUrl = "https://picsum.photos/1200/600",
                 fileUrl = "https://www.google.ca/123.pdf",
-                storeId = ShopControllerTest.STORE_ID,
+                storeId = STORE_ID,
                 price = 1500,
                 currency = "XAF",
                 status = ProductStatus.PUBLISHED,
@@ -61,7 +123,7 @@ class LibraryControllerTest : SeleniumTestSupport() {
                 title = "EXPIRED Product 300",
                 imageUrl = "https://picsum.photos/1200/600",
                 fileUrl = "https://www.google.ca/123.pdf",
-                storeId = ShopControllerTest.STORE_ID,
+                storeId = STORE_ID,
                 price = 1500,
                 currency = "XAF",
                 status = ProductStatus.PUBLISHED,
@@ -72,11 +134,48 @@ class LibraryControllerTest : SeleniumTestSupport() {
         ),
     )
 
+    private val blog = User(
+        id = BLOG_ID,
+        storeId = STORE_ID,
+        walletId = "123",
+        name = "pragmaticdev",
+        fullName = "Pragmatic Dev",
+        email = "pragmaticdev@gmail.com",
+        pictureUrl = "https://picsum.photos/200/200",
+        blog = true,
+        biography = "This is an example of bio",
+        websiteUrl = "https://www.google.ca",
+        language = "en",
+        facebookId = "pragmaticdev",
+        twitterId = "pragmaticdev",
+        publishStoryCount = 10,
+        country = "CM",
+    )
+
+    private val store = Store(
+        id = STORE_ID,
+        userId = BLOG_ID,
+        currency = "XAF",
+        subscriberDiscount = 20,
+        nextPurchaseDiscount = 30,
+        firstPurchaseDiscount = 10,
+        nextPurchaseDiscountDays = 14,
+    )
+
     @BeforeEach
     override fun setUp() {
         super.setUp()
 
         doReturn(SearchBookResponse(books)).whenever(bookBackend).search(any())
+
+        doReturn(GetStoreResponse(store)).whenever(storeBackend).get(STORE_ID)
+
+        doReturn(GetUserResponse(blog)).whenever(userBackend).get(BLOG_ID)
+
+        doReturn(SearchProductResponse(products))
+            .doReturn(SearchProductResponse(products.map { it.copy(storeId = "xxx") }))
+            .whenever(productBackend).search(any())
+        doReturn(SearchOfferResponse(offers)).whenever(offerBackend).search(any())
     }
 
     @Test
@@ -86,6 +185,11 @@ class LibraryControllerTest : SeleniumTestSupport() {
 
         assertCurrentPageIs(PageName.LIBRARY)
         assertElementCount(".book-card", 2)
+
+        Thread.sleep(5000)
+
+        assertElementCount("#library-store-100 .product-card", 2)
+        assertElementCount("#library-other-stores .product-card", 2)
     }
 
     @Test
