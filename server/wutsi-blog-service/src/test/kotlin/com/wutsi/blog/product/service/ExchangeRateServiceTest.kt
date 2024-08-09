ea8fa2c877d611f50c4e5100494b67ad4b777974
wutsi <herve.tchepannou@wutsi.com>
2024-08-09 12:24:28 -0400

    fix linting issues

--- server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/service/ExchangeRateServiceTest.kt
+++ server/wutsi-blog-service/src/test/kotlin/com/wutsi/blog/product/service/ExchangeRateServiceTest.kt
@@ -3,7 +3,6 @@ package com.wutsi.blog.product.service
 import org.junit.jupiter.api.Assertions.assertEquals
 import kotlin.test.Test
 
-
 class ExchangeRateServiceTest {
     val service = ExchangeRateService()
 
