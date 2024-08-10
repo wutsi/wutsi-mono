03b8be375ff2add12693e5a522e5a23ea09bdbec
wutsi <herve.tchepannou@wutsi.com>
2024-08-10 10:10:03 -0400

    update sdk

--- sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/CreateProductCommand.kt
+++ sdk/wutsi-blog-client/src/main/kotlin/com/wutsi/blog/product/dto/CreateProductCommand.kt
@@ -10,7 +10,7 @@ data class CreateProductCommand(
     val categoryId: Long = -1,
     @get:NotEmpty val title: String = "",
     val description: String? = null,
-    @get:Min(1) val price: Long = 0,
+    @get:Min(0) val price: Long = 0,
     val available: Boolean = true,
     val timestamp: Long = System.currentTimeMillis(),
 )
