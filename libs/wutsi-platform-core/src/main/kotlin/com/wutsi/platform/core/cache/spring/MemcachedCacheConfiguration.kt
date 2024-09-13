30d119cc97f9bf47ad826b19485977bac7dcaf95
wutsi <herve.tchepannou@wutsi.com>
2024-09-13 09:34:00 -0400

    fix configuration

--- libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/cache/spring/MemcachedCacheConfiguration.kt
+++ libs/wutsi-platform-core/src/main/kotlin/com/wutsi/platform/core/cache/spring/MemcachedCacheConfiguration.kt
@@ -21,9 +21,9 @@ import org.springframework.context.annotation.Configuration
 )
 open class MemcachedCacheConfiguration(
     @Value("\${wutsi.platform.cache.name}") name: String,
-    @Value(value = "\${wutsi.platform.cache.memcached.username}") private val username: String?,
+    @Value(value = "\${wutsi.platform.cache.memcached.username:}") private val username: String?,
     @Value(value = "\${wutsi.platform.cache.memcached.password:}") private val password: String?,
-    @Value(value = "\${wutsi.platform.cache.memcached.servers:}") private val servers: String,
+    @Value(value = "\${wutsi.platform.cache.memcached.servers}") private val servers: String,
     @Value(value = "\${wutsi.platform.cache.memcached.ttl:86400}") private val ttl: Int,
 ) : AbstractCacheConfiguration(name) {
     @Bean
